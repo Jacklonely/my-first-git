58c698c6e67a226655775cccba199048a472be8b
Jacklonely <Jacklonely14@126.com>
2016-03-03 14:19:28 +0800

    Create TailRecursion.py
    尾递归实现
    n!
    1+2+3+4+...

---
+++ TailRecursion.py
@@ -0,0 +1,13 @@
+#coding=gbk
+
+def factorial(n, result):
+  if n == 0 or n == 1:
+  	return result
+  else:
+  	return factorial(n-1, n*result)
+
+def sum(n, result):
+	if n == 0:
+		return result
+	else:
+		return sum(n-1, n+result)
