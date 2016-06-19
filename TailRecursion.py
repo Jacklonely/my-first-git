9a2c1d1249a74198d97c23135dc0c50967e2566a
yan.wanqiu <jacklonely14@126.com>
2016-06-19 02:22:12 -0700

    sum

--- TailRecursion.py
+++ TailRecursion.py
@@ -1,13 +1,15 @@
 #coding=gbk
 
 def factorial(n, result):
-  if n == 0 or n == 1:
-  	return result
-  else:
+    if n == 0 or n == 1:
+        return result
+    else:
   	return factorial(n-1, n*result)
 
 def sum(n, result):
-	if n == 0:
-		return result
-	else:
-		return sum(n-1, n+result)
+    if n == 0:
+        return result
+    else:
+        return sum(n-1, n+result)
+
+print(sum(4, 0))
